3dbdcfd373e6135c59dd007a6e36fcb8a87da8f4
mojtaba.hosseinalinezhad <mojtaba.alinejad1376@gmail.com>
2026-08-09 21:16:55 +0330

    bug: remove logging parsed url.

--- src/utils/validateUrl.js
+++ src/utils/validateUrl.js
@@ -1,7 +1,6 @@
 export function validateUrl(url) {
   try {
     const parsed = new URL(url);
-    console.log(parsed);
     return parsed.protocol === "http:" || parsed.protocol === "https:";
   } catch (error) {
     console.log(error);
